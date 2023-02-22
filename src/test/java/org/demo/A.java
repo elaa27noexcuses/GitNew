7b6eaa324e668171d4cc1ac0e200ff1034e88b49
elaa27noexcuses <hielaa27@gmailcom>
2023-02-22 17:52:57 +0530

    new code

---
+++ src/test/java/org/demo/A.java
@@ -0,0 +1,13 @@
+package org.demo;
+
+public class A {
+	public void demo() {
+		System.out.println("test 1");
+		System.out.println("test 2");
+		System.out.println("test 3");
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
