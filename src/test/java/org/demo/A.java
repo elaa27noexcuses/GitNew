e84cf5e98fd19da159f7db22f81ed694f57bbeba
munis1 <munis0505@gmail.com>
2023-02-22 18:31:00 +0530

    project munis

--- src/test/java/org/demo/A.java
+++ src/test/java/org/demo/A.java
@@ -5,9 +5,11 @@ public class A {
 		System.out.println("test 1");
 		System.out.println("test 2");
 		System.out.println("test 3");
+		System.out.println("test 6");
+		System.out.println("test 7");
 	}
 	public static void main(String[] args) {
-		
+	
 	}
 
 }
